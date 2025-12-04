fb09b1cf19ae060a5ca432822d6b1ac3ed0c660a
Pushkar7270 <S25CSEU1017@bennett.edu.in>
2025-12-04 16:39:20 +0530

    Bug fixes

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def main():
         while True:
             print('Welcome to the simulation fight')
             print('Choose your character!')
-            print(f'1.{kiana.name},HP {kiana.HP},ATK {kiana.ATK},{kiana.move1_name}:{kiana.Move1},{kiana.move2_name}:{kiana.Move2},{kiana.move3_name}:{kiana.Move3} crit 50% , SPD 40 DEF 20')
-            print(f'2.{mei.name},HP {mei.HP},ATK:{mei.ATK},{mei.move1_name}:{mei.Move1},{mei.move2_name}:{mei.Move2},{mei.move3_name}:{mei.Move3}, crit 30% , SPD 50 DEF 40' )
-            print(f'3.{bronya.name},HP {bronya.HP},ATK {bronya.ATK},{bronya.move1_name}:{bronya.Move1},{bronya.move2_name}:{bronya.Move2},{bronya.move3_name}:{bronya.Move3}, crit 30% , SPD 20 DEF 50' )
+            print(f'1.{kiana.name},HP {kiana.HP},ATK {kiana.BasicATK},{kiana.move1_name}:{kiana.Move1},{kiana.move2_name}:{kiana.Move2},{kiana.move3_name}:{kiana.Move3} crit 50% , SPD 40 DEF 20')
+            print(f'2.{mei.name},HP {mei.HP},ATK:{mei.BasicATK},{mei.move1_name}:{mei.Move1},{mei.move2_name}:{mei.Move2},{mei.move3_name}:{mei.Move3}, crit 30% , SPD 50 DEF 40' )
+            print(f'3.{bronya.name},HP {bronya.HP},ATK {bronya.BasicATK},{bronya.move1_name}:{bronya.Move1},{bronya.move2_name}:{bronya.Move2},{bronya.move3_name}:{bronya.Move3}, crit 30% , SPD 20 DEF 50' )
             print('4. Exit')
             choice = int(input('Enter your choice: '))
             player = None
