adb849bdb56d458a08afee16ce8f872ef904a5f8
Pushkar7270 <S25CSEU1017@bennett.edu.in>
2025-12-11 16:07:00 +0530

    Formatted the code

--- Boss.py
+++ Boss.py
@@ -1,22 +1,41 @@
 import random
+
 import slow_writing as sw
+
+
 class Boss:
-    def __init__(self, name , HP , MAXHP , basicATK,Move1,Move2,Move3,Crit,SPD,DeF,move1_name,move2_name,move3_name):
-            self.name = name
-            self.HP = HP
-            self.MAXHP = MAXHP
-            self.basicATK = basicATK
-            self.Move1 = Move1 
-            self.Move2 = Move2
-            self.Move3 = Move3
-            self.Crit_list = ['Crit'] * Crit + ['Normal'] * (10 - Crit)
-            self.Dodge_list = ['Dodge'] * SPD + ['Normal'] * (10-SPD)
-            self.Parry_list = ['Parry']* DeF + ['Normal'] * (10-DeF)
-            self.move1_name = move1_name
-            self.move2_name = move2_name
-            self.move3_name = move3_name
-    def Attack(self,Target):
-        choices = [1,2,3,4]
+    def __init__(
+        self,
+        name,
+        HP,
+        MAXHP,
+        basicATK,
+        Move1,
+        Move2,
+        Move3,
+        Crit,
+        SPD,
+        DeF,
+        move1_name,
+        move2_name,
+        move3_name,
+    ):
+        self.name = name
+        self.HP = HP
+        self.MAXHP = MAXHP
+        self.basicATK = basicATK
+        self.Move1 = Move1
+        self.Move2 = Move2
+        self.Move3 = Move3
+        self.Crit_list = ["Crit"] * Crit + ["Normal"] * (10 - Crit)
+        self.Dodge_list = ["Dodge"] * SPD + ["Normal"] * (10 - SPD)
+        self.Parry_list = ["Parry"] * DeF + ["Normal"] * (10 - DeF)
+        self.move1_name = move1_name
+        self.move2_name = move2_name
+        self.move3_name = move3_name
+
+    def Attack(self, Target):
+        choices = [1, 2, 3, 4]
         choiced = random.choice(choices)
         critHit = False
         parry = False
@@ -26,57 +45,77 @@ class Boss:
         critchance = random.choice(self.Crit_list)
         parrychance = random.choice(Target.Parry_list)
         dodgechance = random.choice(Target.Dodge_list)
-        if critchance == 'Crit':
+        if critchance == "Crit":
             critHit = True
-        if parrychance == 'Parry':
+        if parrychance == "Parry":
             parry = True
-        if dodgechance == 'Dodge':
+        if dodgechance == "Dodge":
             dodge = True
             hit = False
         if dodge or not hit:
-            sw.print_slow(f'{Target.name} dodged the attack!')
+            sw.print_slow(f"{Target.name} dodged the attack!")
             damage = 0
         if hit:
             if parry:
                 if choiced == 1:
-                    sw.print_slow(f'{self.name} attacked {Target.name} but {Target.name} parried the attack!')
-                    damage = self.basicATK//2
+                    sw.print_slow(
+                        f"{self.name} attacked {Target.name} but {Target.name} parried the attack!"
+                    )
+                    damage = self.basicATK // 2
                 elif choiced == 2:
-                    sw.print_slow(f'{self.name} used {self.move1_name} on {Target.name} but {Target.name} parried the attack')
-                    damage = self.Move1//2
+                    sw.print_slow(
+                        f"{self.name} used {self.move1_name} on {Target.name} but {Target.name} parried the attack"
+                    )
+                    damage = self.Move1 // 2
                 elif choiced == 3:
-                    sw.print_slow(f'{self.name} used {self.move2_name} on {Target.name} but {Target.name} parried the attack')
-                    damage = self.Move2//2
+                    sw.print_slow(
+                        f"{self.name} used {self.move2_name} on {Target.name} but {Target.name} parried the attack"
+                    )
+                    damage = self.Move2 // 2
                 elif choiced == 4:
-                    sw.print_slow(f'{self.name} used {self.move3_name} on {Target.name} but {Target.name} parried the attack')
-                    damage = self.Move3//2
+                    sw.print_slow(
+                        f"{self.name} used {self.move3_name} on {Target.name} but {Target.name} parried the attack"
+                    )
+                    damage = self.Move3 // 2
             elif critHit:
                 if choiced == 1:
-                    sw.print_slow(f'{self.name} attacked {Target.name}')
-                    sw.print_slow('CRITICAL HIT!')
-                    damage = self.basicATK*2
+                    sw.print_slow(f"{self.name} attacked {Target.name}")
+                    sw.print_slow("CRITICAL HIT!")
+                    damage = self.basicATK * 2
                 elif choiced == 2:
-                    sw.print_slow(f'{self.name} used {self.move1_name} on {Target.name}')
-                    damage = self.Move1*2
+                    sw.print_slow(
+                        f"{self.name} used {self.move1_name} on {Target.name}"
+                    )
+                    damage = self.Move1 * 2
                 elif choiced == 3:
-                    sw.print_slow(f'{self.name} used {self.move2_name} on {Target.name}')
-                    damage = self.Move2*2
+                    sw.print_slow(
+                        f"{self.name} used {self.move2_name} on {Target.name}"
+                    )
+                    damage = self.Move2 * 2
                 elif choiced == 4:
-                    sw.print_slow(f'{self.name} used {self.move3_name} on {Target.name}')
-                    damage = self.Move3*2
+                    sw.print_slow(
+                        f"{self.name} used {self.move3_name} on {Target.name}"
+                    )
+                    damage = self.Move3 * 2
             elif not parry and not critHit:
                 if choiced == 1:
-                    sw.print_slow(f'{self.name} attacked {Target.name}')
+                    sw.print_slow(f"{self.name} attacked {Target.name}")
                     damage = self.basicATK
                 elif choiced == 2:
-                    sw.print_slow(f'{self.name} used {self.move1_name} on {Target.name}')
+                    sw.print_slow(
+                        f"{self.name} used {self.move1_name} on {Target.name}"
+                    )
                     damage = self.Move1
                 elif choiced == 3:
-                    sw.print_slow(f'{self.name} used {self.move2_name} on {Target.name}')
+                    sw.print_slow(
+                        f"{self.name} used {self.move2_name} on {Target.name}"
+                    )
                     damage = self.Move2
                 elif choiced == 4:
-                    sw.print_slow(f'{self.name} used {self.move3_name} on {Target.name}')
+                    sw.print_slow(
+                        f"{self.name} used {self.move3_name} on {Target.name}"
+                    )
                     damage = self.Move3
             TotalDamage = damage
-            sw.print_slow(f'{TotalDamage} was dealt out of {Target.HP}')
+            sw.print_slow(f"{TotalDamage} was dealt out of {Target.HP}")
             Target.HP -= TotalDamage
--- slow_writing.py
+++ slow_writing.py
@@ -1,6 +1,8 @@
 import time
+
+
 def print_slow(text):
     for char in text:
-        print(char, end = "", flush = True)
+        print(char, end="", flush=True)
         time.sleep(0.05)
     print()
